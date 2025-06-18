74726af358fd1541ec30e3317a4fa10ae3c0cece
erickSBC <ericks.barr27@gmail.com>
2025-06-18 15:11:36 -0400

    auth ui 2

--- src/app/auth/sign-up/sign-up.component.ts
+++ src/app/auth/sign-up/sign-up.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
 export class SignUpComponent {
+  constructor(private formBuilder: FormBuilder){}
+  form!: FormGroup; 
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      email: ['', Validators.required],  
+      password: ['', [Validators.required, Validators.minLength(3)]],
+      roleSelector: ['Operario', Validators.required]  
 
+    });
+  }
+
+  submit(){
+
+  }
 }
